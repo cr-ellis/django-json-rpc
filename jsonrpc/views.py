dae9a72e2d0ae9b573c233223ec9808d7e8cc9b7
Colin Ellis <panamayacht@gmail.com>
2021-03-11 19:13:37 -0500

    Replace obsolete render_to_string

--- jsonrpc/views.py
+++ jsonrpc/views.py
@@ -1,6 +1,6 @@
 from jsonrpc._json import dumps
 from django.http import HttpResponse
-from django.shortcuts import render_to_response
+from django.shortcuts import render
 from jsonrpc.site import jsonrpc_site
 from jsonrpc import mochikit
 
@@ -13,8 +13,9 @@ def browse(request):
         return HttpResponse(mochikit.interpreter,
                             content_type='application/x-javascript')
     desc = jsonrpc_site.service_desc()
-    return render_to_response('browse.html', {
-        'methods': desc['procs'],
-        'method_names_str': dumps(
-            [m['name'] for m in desc['procs']])
+    return render(request,
+        'browse.html', {
+            'methods': desc['procs'],
+            'method_names_str': dumps(
+                [m['name'] for m in desc['procs']])
     })
